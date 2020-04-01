341b76dafe90624dd1cf6eb2b8f6c6b5254a68e9
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-04-01 11:14:18 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
+import { ReactComponent as Sun} from './assets/sun.svg'
+import { ReactComponent as ProcessBuilding} from './assets/process_building.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  render() {
+    return(
+    <Parallax ref={ ref => (this.parallax = ref)} pages={3} >
+       <ParallaxLayer offset={0} speed={0}  style={{ backgroundColor: '#702632' }} />
+       <ParallaxLayer offset={1} speed={0}  style={{ backgroundColor: '#40434E' }} />
+       <ParallaxLayer offset={2} speed={0}  style={{ backgroundColor: '#381460' }} />
+
+       <ParallaxLayer offset={1.15} speed={0.7}  style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+         <Sun style={{ width: '20%', marginLeft: '20%'}}/>
+       </ParallaxLayer>
+       <ParallaxLayer offset={0.15} speed={0.3}  style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+         <ProcessBuilding style={{ width: '20%', marginLeft: '70%'}}/>
+       </ParallaxLayer>
+
+
+       <ParallaxLayer offset={0} speed={-0.3} onClick={() => this.parallax.scrollTo(1)} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <h1 style={{width: '80%'}}>Hello there! this is my portfolio website</h1>
+       </ParallaxLayer>
+       <ParallaxLayer offset={1} speed={0.3} onClick={() => this.parallax.scrollTo(2)} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+        <h1 style={{width: '80%'}}>And these are my projects!</h1>
+        <p style={{width: '80%'}}>Most of them involve Javacript, specifically ReactJS which I find quite an exciting and powerful library</p>
+       </ParallaxLayer>
+       <ParallaxLayer offset={2} speed={0.3} onClick={() => this.parallax.scrollTo(0)} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <h1 style={{width: '80%'}}>Feel free to contact me here:</h1>
+       </ParallaxLayer>
+    </Parallax>
   );
-}
+}}
 
 export default App;
