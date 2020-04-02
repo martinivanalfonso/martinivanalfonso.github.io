986e5d93fff2156b1d428a154547483bde333dc2
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-04-02 16:59:19 -0300

    improved parallax and project-block component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
-import ProjectsBlockOne from './components/projects-blocks/projects-block-one.component'
-import ProjectsBlockTwo from './components/projects-blocks/projects-block-two.component'
-import styled from 'styled-components'
+import ProjectsBlock from './c./components/projects-block/projects-block.component'
+import HeaderBackground from './components/header-background/header-background.component'
 
 import { ReactComponent as Sun} from './assets/sun.svg'
 import { ReactComponent as ProcessBuilding} from './assets/process_building.svg'
 import { ReactComponent as PieChart} from './assets/pie_chart.svg'
 import { ReactComponent as Mountain} from './assets/mountain.svg'
 import { ReactComponent as DataProcess} from './assets/data_process.svg'
-
-import CropSquareIcon from '@material-ui/icons/CropSquare';
-import ChangeHistoryIcon from '@material-ui/icons/ChangeHistory';
-import CodeIcon from '@material-ui/icons/Code';
+import { ReactComponent as Cloud} from './assets/cloud.svg'
 
 class App extends React.Component {
   render() {
-    const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
-const Pink = ({ children }) => <span style={{ color: '#FF6AC1' }}>{children}</span>
-const Yellow = ({ children }) => <span style={{ color: '#EFF59B' }}>{children}</span>
-const Lightblue = ({ children }) => <span style={{ color: '#9AEDFE' }}>{children}</span>
-const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</span>
-const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span>
-const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
-
-const IconsContainer = styled.div`
-animation: 
-    nudge 5s linear infinite alternate;
-}
-
-@keyframes nudge {
-  0%, 100% {
-    transform: translate(0, 0);
-  }
-  50% {
-    transform: translate(0, 10px);
-  }
-  80% {
-    transform: translate(0, -10px);
-  }
-}
-`
-
     return(
     <Parallax ref={ ref => (this.parallax = ref)} pages={4} >
        <ParallaxLayer offset={0} speed={0}  style={{ backgroundColor: '#702632' }} />
@@ -51,18 +21,17 @@ animation:
        <ParallaxLayer offset={3} speed={0}  style={{ backgroundColor: '#381460' }} />
 
        <ParallaxLayer offset={0} speed={0.3}>
-         <IconsContainer>
-         <CropSquareIcon style={{ width: '40%', fontSize:'4rem', marginTop: '5%'}}/>
-         <ChangeHistoryIcon style={{ width: '60%', marginLeft: '60%', fontSize:'8rem'}}/>
-         <CodeIcon style={{width: '20%', fontSize:'4rem', marginTop: `${window.innerWidth < 960 ?  '50vh' : '20vh' }`}}/>
-         </IconsContainer>
+        <HeaderBackground />
        </ParallaxLayer>
 
-       <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.7 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+       <ParallaxLayer offset={0} speed={0.8} style={{ opacity: 0.4 }}>
+          <Cloud style={{ display: 'block', width: '20%', marginLeft: '25%' }} />
+          <Cloud style={{ display: 'block', width: '10%', marginLeft: '45%' }} />
         </ParallaxLayer>
-
+       <ParallaxLayer offset={1} speed={-0.2} style={{ opacity: 0.7 }}>
+          <Cloud style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <Cloud style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+        </ParallaxLayer>
 
        <ParallaxLayer offset={0.2} speed={0.3}  style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
          <PieChart style={{ width: '20%', marginLeft: '60%'}}/>
@@ -80,20 +49,13 @@ animation:
          <Mountain style={{ width: '20%', marginLeft: '60%'}}/>
        </ParallaxLayer>
 
-
-
        <ParallaxLayer offset={0} speed={-0.2} onClick={() => this.parallax.scrollTo(1)} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <h1 style={{width: '80%'}}>Hello there!<br />I`m a front-end developer, this is my portfolio website</h1>
        </ParallaxLayer>
        <ParallaxLayer offset={1} speed={0.3} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         <h1 style={{width: '80%'}}>And these are my projects!</h1>
         <p style={{width: '80%'}}>Most of them involve Javacript, specifically ReactJS which I find quite an exciting and powerful library</p>
-       </ParallaxLayer>
-       <ParallaxLayer offset={`${window.innerWidth < 960 ?  '1.7' : '1.3' }`} speed={0.3} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-        <ProjectsBlockOne />
-       </ParallaxLayer>
-       <ParallaxLayer offset={`${window.innerWidth < 960 ?  '2.4' : '2' }`} speed={0.3} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-        <ProjectsBlockTwo />
+        <ProjectsBlock />
        </ParallaxLayer>
        <ParallaxLayer offset={3} speed={0.3} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         <h1 style={{width: '80%'}}>Feel free to contact me here:</h1>
--- src/components/projects-block/projects-block.component.jsx
+++ src/components/projects-block/projects-block.component.jsx
@@ -6,9 +6,10 @@ import styled from 'styled-components'
 const ProjectBlockOneContainer = styled.div`
 display: grid;
 grid-template-columns: ${window.innerWidth < 960 ? "1fr" : "1fr 1fr"};
-grid-gap: 10vw;
+grid-gap: 40px;
 width: 90%;
 justify-items: center;
+margin-top: 20px;
 `;
 
 const ProjectsBlockone = () => (
@@ -25,6 +26,23 @@ const ProjectsBlockone = () => (
       info="Interactive website created to store logic problems and their solution"
       link="https://martinivanalfonso.github.io/MyProblemCloud/"
     />
+        <MediaCard
+      imageUrl="https://i.ibb.co/LQN4hVs/monstersrolodex.png"
+      title="Monsters Rolodex"
+      info="Creative feature implementing a search functional component."
+      link="https://martinivanalfonso.github.io/MonstersRolodex/"
+    />
+    {
+      window.innerWidth < 960 ?
+      ''
+      :
+    <MediaCard
+      imageUrl="https://i.ibb.co/tLct13h/spu-ea68c8-ogi2-3cwn3bmfojjlb56e.jpg"
+      title="Vitality Shot"
+      info="Modern website created to introduce a product using  user friendly animation"
+      link="https://martinivanalfonso.github.io/ProductPage/"
+    />
+    }
   </ProjectBlockOneContainer>
 );
 
