4e2f8dad48a87b9dd762ac71f2a02e1760ee701f
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-04-25 14:20:17 -0300

    Updates

---
+++ precache-manifest.4f850859ec795db9c87660358048260b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ab8f5d26bb01f479beb584ec594ba2",
+    "url": "/martinivanalfonso.github.io/index.html"
+  },
+  {
+    "revision": "b522d1c9390662b821f7",
+    "url": "/martinivanalfonso.github.io/static/css/main.ed5e4874.chunk.css"
+  },
+  {
+    "revision": "6c0ba49002b9e157125f",
+    "url": "/martinivanalfonso.github.io/static/js/2.b5933a04.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/martinivanalfonso.github.io/static/js/2.b5933a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b522d1c9390662b821f7",
+    "url": "/martinivanalfonso.github.io/static/js/main.807820dd.chunk.js"
+  },
+  {
+    "revision": "955dc512579462418c06",
+    "url": "/martinivanalfonso.github.io/static/js/runtime-main.60adf782.js"
+  },
+  {
+    "revision": "305b7b7bf17b8341d4c61d0dc017fb9d",
+    "url": "/martinivanalfonso.github.io/static/media/cloud.305b7b7b.svg"
+  },
+  {
+    "revision": "f9c8b12757db632cbd634271b1cd5b10",
+    "url": "/martinivanalfonso.github.io/static/media/data_analytics_.f9c8b127.svg"
+  },
+  {
+    "revision": "8c7ae19756b45657b73055a1142be613",
+    "url": "/martinivanalfonso.github.io/static/media/data_process.8c7ae197.svg"
+  },
+  {
+    "revision": "54c3adc538f06c7819b16b934f876911",
+    "url": "/martinivanalfonso.github.io/static/media/mountain.54c3adc5.svg"
+  },
+  {
+    "revision": "921df655e6f1ce400074caecfdf30605",
+    "url": "/martinivanalfonso.github.io/static/media/process_building.921df655.svg"
+  },
+  {
+    "revision": "fafe0d09bc8d6a2b0848c96d52b68f36",
+    "url": "/martinivanalfonso.github.io/static/media/sun.fafe0d09.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.b38bcab8e2f2403979226641b920edee.js"
+  "/martinivanalfonso.github.io/precache-manifest.4f850859ec795db9c87660358048260b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/martinivanalfonso.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
